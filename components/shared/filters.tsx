6bb659bf67641b3d0f705ec05a0e691a41288a7e
kenali <kanan1294@gmail.com>
2025-03-18 16:51:15 +0200

    add checkbox-filters-group

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -14,11 +14,13 @@ export const Filters: React.FC<Props> = ({ className }) => {
     <div className={className}>
       <Title text="Фильтрация" size="sm" className="mb-5 font-bold" />
 
+      {/* Верхние чекбоксы */}
       <div className="flex flex-col gap-4">
         <FilterCheckbox text="Можно собирать" value="1" />
         <FilterCheckbox text="Новинки" value="2" />
       </div>
 
+      {/* Фильтр цен */}
       <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
         <p className="font-bold mb-3">Цена от и до:</p>
         <div className="flex gap-3 mb-5">
@@ -32,7 +34,7 @@ export const Filters: React.FC<Props> = ({ className }) => {
           <Input type="number" placeholder="1000" min={100} max={1000} />
         </div>
 
-        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]}/>
+        <RangeSlider min={0} max={5000} step={10} value={[0, 5000]} />
       </div>
     </div>
   );
