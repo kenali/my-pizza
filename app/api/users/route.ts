999778267db86b9b8b5ecfc58c1426dc2c2d71fd
kenali <kanan1294@gmail.com>
2026-04-16 23:27:06 +0300

    extend prisma schema

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -8,8 +8,6 @@ export async function GET() {
 
 export async function POST(req: NextRequest) {
   const data = await req.json();
-  console.log("DATA:", data);
   const user = await prisma.user.create({ data });
-
   return NextResponse.json(user);
 }
