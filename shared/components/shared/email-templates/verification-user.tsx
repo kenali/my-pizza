50aad036644b785253760440b81a57efd2dd30de
kenali <kanan1294@gmail.com>
2026-05-14 22:16:51 +0300

    finished fixed

--- shared/components/shared/email-templates/verification-user.tsx
+++ shared/components/shared/email-templates/verification-user.tsx
@@ -3,14 +3,19 @@ interface Props {
 }
 
 export const VerificationUserTemplate = ({ code }: Props) => {
+
+  const domain = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : process.env.NEXT_PUBLIC_URL || "http://localhost:3000";
+    
   return (
     <div>
       <p>
-        Код подтверждения: <h2>{code}</h2>
+        Код подтверждения: <b>{code}</b>
       </p>
 
       <p>
-        <a href={`http://localhost:3000/api/auth/verify?code=${code}`}>
+        <a href={`${domain}/api/auth/verify?code=${code}`}>
           Подтвердить регистрацию
         </a>
       </p>
