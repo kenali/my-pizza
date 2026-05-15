28ea026e4ff40e3d865f8bc423073975e8512f3c
kenali <kanan1294@gmail.com>
2026-05-15 13:35:28 +0300

    fixed seed issue

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -209,7 +209,8 @@ async function down() {
   await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
   await prisma.$executeRaw`TRUNCATE TABLE "Ingredient" RESTART IDENTITY CASCADE`;
   await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
-  await prisma.$executeRaw`TRUNCATE TABLE "ProductItem" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "Story" RESTART IDENTITY CASCADE`;
+  await prisma.$executeRaw`TRUNCATE TABLE "StoryItem" RESTART IDENTITY CASCADE`;
 }
 async function main() {
   try {
